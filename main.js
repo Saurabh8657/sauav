90ea09fa26952632f1eae9ce37c043de0f0dd2d3
Sourav Anand <sauravanand324@gmail.com>
2024-01-19 00:39:26 +0530

    api done

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const baseURL = 'http://localhost:3000/';
+
+async function fetchAndDisplayDestinations() {
+    try {
+        const response = await fetch(baseURL + 'destinations');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const destinations = await response.json();
+        console.log(destinations);
+        displayDestinations(destinations);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function displayDestinations(destinations) {
+    const destinationList = document.getElementById('destinationList');
+
+    destinationList.innerHTML = '';
+
+    destinations.forEach(destination => {
+        const destinationItem = document.createElement('div');
+        destinationItem.innerHTML = `<p>${destination.city}, ${destination.country}</p>`;
+        destinationList.appendChild(destinationItem);
+    });
+}
+
+fetchAndDisplayDestinations();
